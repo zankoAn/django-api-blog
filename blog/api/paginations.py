8fa09555a329606a78f39d182db6f00d99d20377
zankoAn <zankoan4@gmail.com>
2024-10-04 11:57:14 +0330

    increase pagination size limit

--- blog/api/paginations.py
+++ blog/api/paginations.py
@@ -3,7 +3,8 @@ from rest_framework import pagination
 
 class ArticlePagination(pagination.CursorPagination):
     ordering = "-published"
-    page_size = 5
+    page_size = 6
+    max_page_size = 8
     page_size_query_param = "page_size"
 
 
@@ -11,6 +12,7 @@ class CategoryPagination(pagination.PageNumberPagination):
     ordering = "-published"
     page_size = 5
     page_size_query_param = "page_size"
+    max_page_size = 20
 
 
 class UserPagination(pagination.CursorPagination):
