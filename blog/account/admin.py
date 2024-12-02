03523584de375e7e8c7981bc488082abe55bbb55
zankoAn <zankoan4@gmail.com>
2024-12-02 12:13:08 +0330

    Replace ModelAdmin with BaseUserAdmin

--- blog/account/admin.py
+++ blog/account/admin.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from blog.account.models import User, Resume, WorkExperience
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ("id", "username", "email", "is_active", "phone_number")
     list_display_links = ("id", "username", "phone_number")
     list_editable = ("is_active",)
     search_fields = ("username", "email", "is_active")
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "username",
+                    "email",
+                    "phone_number",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+    )
 
 
 class WorkExperienceInline(admin.StackedInline):
@@ -21,5 +39,3 @@ class ResumeAdmin(admin.ModelAdmin):
     inlines = [WorkExperienceInline]
     list_display = ("id", "user", "current_position")
     list_display_links = ("id", "current_position")
-
-admin.site.register(WorkExperience)
